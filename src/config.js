ec4b66cc7aa94a2cdcbd77dead86f6df12ca32bf
Navdeep Singh <navdeep.er@gmail.com>
2019-07-03 20:38:51 +0530

    Update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,13 @@
 const config = {};
 
-config.API_BASE_URL = (process.env.API_URL !== undefined) ? process.env.API_URL : 'http://localhost:8000';
+config.production = {
+  'API_BASE_URL': process.env.API_URL
+}
+config.default = {
+  'API_BASE_URL': 'http://localhost:8000'
+}
 
-export default config;
+
+const chosenConfig = process.env.NODE_ENV === 'production' ? config.production : config.default;
+
+export default chosenConfig;
