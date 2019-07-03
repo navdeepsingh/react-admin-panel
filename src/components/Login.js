56601a3c1e1afe46ef6495826680956a08b9fee3
Navdeep Singh <navdeep.er@gmail.com>
2019-07-03 12:16:10 +0530

    Update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import '../css/Login.css';
+import config from '../config';
 
 class Login extends React.Component {
 
+
+	email = React.createRef();
+	password = React.createRef();
+
 	login = (e) => {
 		e.preventDefault();
-		console.log(this);
+		this.setState({
+			error: false,
+			errorMessage: ''
+		});
+		const emailValue = this.email.current.value;
+		const passwordValue = this.password.current.value;
+		fetch(config.API_BASE_URL + '/api/login', {
+			method: 'POST',
+			mode: 'no-cors',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer`,
+				Accept: 'application/json'
+			},
+			body: JSON.stringify({
+				"email": emailValue,
+				"password": passwordValue
+			})
+		}).then(response => {
+			if (response.status < 200 || response.status >= 300) {
+				//throw new Error(response.statusText);
+				console.log('Error');
+				return;
+			}
+			return response.json();
+		})
 	}
 
 	render() {
@@ -18,12 +48,12 @@ class Login extends React.Component {
 								<h5 className="card-title text-center">Sign In</h5>
 								<form className="form-signin" onSubmit={this.login}>
 									<div className="form-label-group">
-										<input type="email" ref={} id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
+										<input type="email" ref={this.email} id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
 										<label htmlFor="inputEmail">Email address</label>
 									</div>
 
 									<div className="form-label-group">
-										<input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+										<input type="password" ref={this.password} id="inputPassword" className="form-control" placeholder="Password" required />
 										<label htmlFor="inputPassword">Password</label>
 									</div>
 
