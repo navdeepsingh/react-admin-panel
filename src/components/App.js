57664711dffafc2f7044d9aa197e237fd062dc83
Navdeep Singh <navdeep.er@gmail.com>
2019-07-03 11:24:36 +0530

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/App.css';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h3>Default Page</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="text-center">Page Not Found</div>
+);
+
+export default NotFound;
